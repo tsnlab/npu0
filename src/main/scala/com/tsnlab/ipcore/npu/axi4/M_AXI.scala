dad8ac0f2a524b20375c839f3f0c050615596f05
Yonghyu Ban <yonghyu@tsnlab.com>
2022-03-10 11:02:17 +0900

    Final definition for AXI4 master bus

--- src/main/scala/com/tsnlab/ipcore/npu/axi4/M_AXI.scala
+++ src/main/scala/com/tsnlab/ipcore/npu/axi4/M_AXI.scala
@@ -14,23 +14,75 @@ class M_AXI(axi4param: AXI4Param) extends Module {
   val axiWriteState = RegInit(AXI4WriteState.AWVALID)
 
   // Define some registers for the AXI output
-  // Bus signals
-  val axi_awvalid = RegInit(0.B)
-  val axi_arvalid = RegInit(0.B)
-  val axi_wvalid  = RegInit(0.B)
-  val axi_rready  = RegInit(0.B)
 
-  // Bus data
+  // AR channel
+  val axi_araddr  = RegInit(0.U(axi4param.addrWidth.W))
+  val axi_arlen   = RegInit(0.U(axi4param.burstWidth.W))
   val axi_arsize  = RegInit(0.U(3.W))
   val axi_arburst = RegInit(0.U(2.W))
   val axi_arlock  = RegInit(0.U(2.W))
   val axi_arcache = RegInit(0.U(4.W))
+  val axi_arprot  = RegInit(0.U(3.W))
+  val axi_arid    = RegInit(0.U(axi4param.idWidth.W))
+  val axi_arqos   = RegInit(0.U(4.W))
+  val axi_arvalid = RegInit(0.B)
 
-  // Wire up them to the output
-  M_AXI.awvalid := axi_awvalid
-  M_AXI.arvalid := axi_arvalid
-  M_AXI.wvalid := axi_wvalid
-  M_AXI.rready := axi_rready
+  M_AXI.araddr   := axi_araddr
+  M_AXI.arlen    := axi_arlen
+  M_AXI.arsize   := axi_arsize
+  M_AXI.arburst  := axi_arburst
+  M_AXI.arlock   := axi_arlock
+  M_AXI.arcache  := axi_arcache
+  M_AXI.arprot   := axi_arprot
+  M_AXI.arid     := axi_arid
+  M_AXI.arqos    := axi_arqos
+  M_AXI.arvalid  := axi_arvalid
+
+  // AW channel
+  val axi_awaddr  = RegInit(0.U(axi4param.addrWidth.W))
+  val axi_awlen   = RegInit(0.U(axi4param.burstWidth.W))
+  val axi_awsize  = RegInit(0.U(3.W))
+  val axi_awburst = RegInit(0.U(2.W))
+  val axi_awlock  = RegInit(0.U(2.W))
+  val axi_awcache = RegInit(0.U(4.W))
+  val axi_awprot  = RegInit(0.U(3.W))
+  val axi_awid    = RegInit(0.U(axi4param.idWidth.W))
+  val axi_awqos   = RegInit(0.U(4.W))
+  val axi_awvalid = RegInit(0.B)
+
+  M_AXI.awaddr   := axi_awaddr
+  M_AXI.awlen    := axi_awlen
+  M_AXI.awsize   := axi_awsize
+  M_AXI.awburst  := axi_awburst
+  M_AXI.awlock   := axi_awlock
+  M_AXI.awcache  := axi_awcache
+  M_AXI.awprot   := axi_awprot
+  M_AXI.awid     := axi_awid
+  M_AXI.awqos    := axi_awqos
+  M_AXI.awvalid  := axi_awvalid
+
+  // W channel
+  val axi_wdata   = RegInit(0.U(axi4param.dataWidth.W))
+  val axi_wstrb   = RegInit(0.U(((axi4param.dataWidth + 7) / 8).W)) // See A10.3.4
+  val axi_wlast   = RegInit(0.B)
+  val axi_wvalid  = RegInit(0.B)
+
+  M_AXI.wdata    := axi_wdata
+  M_AXI.wstrb    := axi_wstrb
+  M_AXI.wlast    := axi_wlast
+  M_AXI.wvalid   := axi_wvalid
+
+  // R channel
+  val axi_rlast   = RegInit(0.B)
+  val axi_rready  = RegInit(0.B)
+
+  M_AXI.rlast    := axi_rlast
+  M_AXI.rready   := axi_rready
+
+  // B channel
+  val axi_bready  = RegInit(0.B)
+  M_AXI.bready   := axi_bready
+  // Wire up bus data
 
   // First, define the state machine for read
   switch (axiReadState) {
