e61675db60c82bf4943152b8815e5c903848a74e
Yonghyu Ban <yonghyu@tsnlab.com>
2022-03-31 18:10:32 +0900

    AXI master bus communication test (basic)

---
+++ src/test/scala/com/tsnlab/ipcore/npu/AXIMasterTest.scala
@@ -0,0 +1,85 @@
+package com.tsnlab.ipcore.npu
+
+import chisel3._
+import chisel3.tester.testableClock
+import chiseltest.{ChiselScalatestTester, testableUInt}
+import chiseltest.simulator.WriteVcdAnnotation
+import org.scalatest.freespec.AnyFreeSpec
+import com.tsnlab.ipcore.axi4._
+import com.tsnlab.ipcore.npu.util.BusState._
+
+// Most tests are AXI4 compatible, does not fully test AXI3 yet
+class AXIMasterTest extends AnyFreeSpec with ChiselScalatestTester {
+  val axiparam = AXI4Param (
+    idWidth = 12,
+    addrWidth = 32,
+    dataWidth = 32,
+    awuserWidth = 2,
+    aruserWidth = 2,
+    wuserWidth = 0,
+    ruserWidth = 0,
+    buserWidth = 0,
+    busvariant = AXIVariant.AXI3, // ZYNQ-7000 Uses AXI3
+  )
+
+  "AXI Master - Simulate and test bus read" in {
+    test(new M_AXI(axiparam)).withAnnotations(Seq(WriteVcdAnnotation)) {
+      aximaster => {
+        var busslaveState: BusState = ARREADY
+
+        // Some default values
+        aximaster.memport_r.addr.poke("hCA7F_00D0".U)
+        aximaster.memport_w.data.poke("hDEAD_F00D".U)
+
+        // Kick it!
+        aximaster.memport_r.enable.poke(1.B)
+
+        var id: BigInt = 0
+
+        for (i <- 1 to 512) {
+          aximaster.clock.step()
+
+          busslaveState match {
+            case ARREADY => {
+              aximaster.M_AXI.arready.poke(1.B)
+
+              if (aximaster.M_AXI.arvalid.peek().litValue == 1) {
+                // If it is not a catfood, something gone wrong
+                aximaster.M_AXI.araddr.expect("hCA7F_00D0".U)
+                id = aximaster.M_AXI.arid.peek().litValue
+                busslaveState = RREADY
+              }
+            }
+
+            case RREADY => {
+              aximaster.M_AXI.arready.poke(0.B)
+              aximaster.M_AXI.rvalid.poke(1.B)
+              aximaster.M_AXI.rdata.poke("hDEAD_F00D".U)
+              aximaster.M_AXI.rid.poke(id.U)
+              aximaster.M_AXI.rlast.poke(1.B)
+
+              if (aximaster.M_AXI.rready.peek().litValue == 1) {
+                // Prepare for RDATA
+                busslaveState = NOOP
+              }
+            }
+
+            case NOOP => {
+              aximaster.M_AXI.rvalid.poke(0.B)
+              aximaster.memport_r.enable.poke(0.B)
+            }
+          }
+        }
+      }
+    }
+  }
+
+  "AXI Master - Simulate and test bus write" in {
+    test(new M_AXI(axiparam)).withAnnotations(Seq(WriteVcdAnnotation)) {
+      aximaster => {
+        case ARREADY
+
+      }
+    }
+  }
+}
