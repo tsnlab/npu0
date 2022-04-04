4d46fed2833cf09077e73ea06607c9cd6a9dbee0
Yonghyu Ban <yonghyu@tsnlab.com>
2022-04-04 16:04:50 +0900

    Hacky reset generator to make it work properly for... now...

--- src/main/scala/com/tsnlab/ipcore/npu/ClockTree.scala
+++ src/main/scala/com/tsnlab/ipcore/npu/ClockTree.scala
@@ -21,13 +21,19 @@ class ClockTree extends Module {
   clko.div4 := divreg(1)
   clko.div8 := divreg(2)
 
+  val rstcnt = RegInit("b11111".U(5.W))
+  rstcnt := rstcnt - (rstcnt(4) & 1.U)
+
   val rst2 = RegInit(1.B)
   val rst4 = RegInit(1.B)
   val rst8 = RegInit(1.B)
 
-  clko.rst2 := rst2
-  clko.rst4 := rst4
-  clko.rst8 := rst8
+  //clko.rst2 := rst2 || reset.asBool
+  //clko.rst4 := rst4 || reset.asBool
+  //clko.rst8 := rst8 || reset.asBool
+  clko.rst2 := rstcnt(4)
+  clko.rst4 := rstcnt(4)
+  clko.rst8 := rstcnt(4)
 
   withClock((~clko.div2).asClock) {
     rst2 := 0.B
