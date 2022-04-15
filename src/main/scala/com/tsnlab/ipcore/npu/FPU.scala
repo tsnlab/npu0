c3db96d23a7561dd47b19e58f36ab0d1251acb87
Yonghyu Ban <yonghyu@tsnlab.com>
2022-04-15 16:27:17 +0900

    Hold FPU input data for combinational FPUs

--- src/main/scala/com/tsnlab/ipcore/npu/FPU.scala
+++ src/main/scala/com/tsnlab/ipcore/npu/FPU.scala
@@ -28,6 +28,11 @@ class FPU(exponent: Int, mantissa: Int) extends Module {
     val o_valid = Output(Bool())
   })
 
+  val reg_a = RegInit(0.U(data.a.getWidth.W))
+  val reg_b = RegInit(0.U(data.b.getWidth.W))
+  val reg_y = RegInit(0.U(data.y.getWidth.W))
+  val current_op = RegInit(FPUOperand.ADD)
+
   val i_ready_reg = RegInit(1.B)
   val o_valid_reg = RegInit(0.B)
 
@@ -35,52 +40,51 @@ class FPU(exponent: Int, mantissa: Int) extends Module {
   val fdiv = Module(new FDIV(exponent, mantissa + 1))
   val fmul = Module(new FMUL(exponent, mantissa + 1))
 
-  fadd.io.a  := data.a
-  fdiv.io.a  := data.a
-  fmul.io.a  := data.a
+  fadd.io.a  := reg_a
+  fdiv.io.a  := reg_a
+  fmul.io.a  := reg_a
 
-  when (control.op === 1.U) {
-    fadd.io.b := Cat(!data.b(exponent + mantissa),
-                     data.b(exponent + mantissa - 1, 0))
+  when (current_op === FPUOperand.SUB) {
+    fadd.io.b := Cat(!reg_b(exponent + mantissa),
+                     reg_b(exponent + mantissa - 1, 0))
   } otherwise {
-    fadd.io.b := data.b
+    fadd.io.b := reg_b
   }
-  fdiv.io.b  := data.b
-  fmul.io.b  := data.b
+  fdiv.io.b  := reg_b
+  fmul.io.b  := reg_b
   fadd.io.rm := 0.U
   fdiv.io.rm := 0.U
   fmul.io.rm := 0.U
 
-  // Wire control signal to fdiv value
-  fdiv.io.specialIO.in_valid := MuxLookup(control.op, 0.B, Array(
-    FPUOperand.DIV -> control.i_valid
-  ))
-  fdiv.io.specialIO.out_ready := MuxLookup(control.op, 0.B, Array(
-    FPUOperand.DIV -> control.o_ready
-  ))
-  control.i_ready := MuxLookup(control.op, i_ready_reg, Array(
-    FPUOperand.DIV -> fdiv.io.specialIO.in_ready
-  ))
-  control.o_valid := MuxLookup(control.op, o_valid_reg, Array(
-    FPUOperand.DIV -> fdiv.io.specialIO.out_valid
-  ))
-
   fdiv.io.specialIO.kill := 0.B
   fdiv.io.specialIO.isSqrt := 0.B
-
-  data.y := MuxLookup(control.op, "hFFFF_FFFF".U, Array(
-    FPUOperand.ADD -> fadd.io.result,
-    FPUOperand.SUB -> fadd.io.result,
-    FPUOperand.MUL -> fmul.io.result,
-    FPUOperand.DIV -> fdiv.io.result,
-  ))
 
   // Support for decoupled clock signal
   val state = RegInit(FPUState.READY)
   val clkcnt = RegInit(0.U(BitUtil.getBitWidth(16).W))
-  val current_op = RegInit(FPUOperand.ADD)
 
-  val clkdelay: Int = 2;
+  // Wire control signal to fdiv value
+  fdiv.io.specialIO.in_valid := MuxLookup(current_op, 0.B, Array(
+    FPUOperand.DIV -> 1.B //control.i_valid
+  ))
+  fdiv.io.specialIO.out_ready := MuxLookup(current_op, 0.B, Array(
+    FPUOperand.DIV -> control.o_ready
+  ))
+
+  // TODO: 여기 valid/ready 찔러봐야 함, Simulation task
+  control.i_ready := MuxLookup(current_op, i_ready_reg, Array(
+    FPUOperand.DIV -> fdiv.io.specialIO.in_ready
+  ))
+  control.o_valid := MuxLookup(current_op, o_valid_reg, Array(
+    FPUOperand.DIV -> fdiv.io.specialIO.out_valid
+  ))
+
+  //data.y := reg_y
+  data.y := MuxLookup(current_op, reg_y, Array(
+    FPUOperand.DIV -> fdiv.io.result
+  ))
+  
+  val clkdelay: Int = 1;
 
   switch (state) {
     is (FPUState.READY) {
@@ -91,6 +95,9 @@ class FPU(exponent: Int, mantissa: Int) extends Module {
           o_valid_reg := 0.B
         }
         current_op := control.op
+        reg_a := data.a
+        reg_b := data.b
+        i_ready_reg := 0.B
         state := FPUState.PROCESS
       } 
     }
@@ -100,6 +107,12 @@ class FPU(exponent: Int, mantissa: Int) extends Module {
         when (clkcnt >= clkdelay.U) {
           o_valid_reg := 1.B
           i_ready_reg := 1.B
+          reg_y := MuxLookup(current_op, DontCare, Array(
+            FPUOperand.ADD -> fadd.io.result,
+            FPUOperand.SUB -> fadd.io.result,
+            FPUOperand.MUL -> fmul.io.result,
+          ))
+
         } otherwise {
           clkcnt := clkcnt + 1.U
           i_ready_reg := 0.B
